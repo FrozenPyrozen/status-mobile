37a64ce6d03ddffe756a5946100e3edf4129f63e
FrozenPyrozen <kiril.kohan@gmail.com>
2023-06-19 09:53:08 +0300

    commented styles for network_connections

--- src/quo2/components/network_connections/style.cljs
+++ src/quo2/components/network_connections/style.cljs
@@ -1,12 +1,14 @@
 (ns quo2.components.network-connections.style
-  (:require [quo2.foundations.colors :as colors]))
+  (:require [quo2.foundations.colors :as colors]
+            [utils.re-frame :as rf]))
 
-(def item-width :dimensions/window-width)
+;; (def window-width (rf/sub [:dimensions/window-width]))
 
 (def container
   {:flex-direction     :row
    :align-items        :center
    :justify-content    :space-between
+;;   :background-color   :red
    :padding-horizontal 10
    :margin-bottom      20})
 
@@ -15,15 +17,18 @@
    :align-items :flex-start})
 
 (def item-container
-  {:width              item-width
+  {:width              300
    :height             44
    :flex               1
    :flex-direction     :column
    :align-items        :flex-start
    :border-radius      12
    :border-width       1
+;;   :border-color       :green
    :padding-horizontal 8
-   :padding-vertical   5})
+   :padding-vertical   5
+;;  :background-color   :grey
+  })
 
 (def item-row
   {:flex-direction :row
@@ -45,9 +50,8 @@
   })
 
 (def icon
-  {:width            12
-   :height           12
-   :background-color :black})
+  {:width  12
+   :height 12})
 
 (def title
   {})
