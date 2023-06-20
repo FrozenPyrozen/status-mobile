bfefb2afe9acdc6b77a0fbca7294b5638e9e51c0
FrozenPyrozen <kiril.kohan@gmail.com>
2023-06-20 10:49:09 +0300

    added styling for item in `network-connections`

--- src/quo2/components/network_connections/style.cljs
+++ src/quo2/components/network_connections/style.cljs
@@ -1,14 +1,13 @@
 (ns quo2.components.network-connections.style
   (:require [quo2.foundations.colors :as colors]
-            [utils.re-frame :as rf]))
+  ))
 
-;; (def window-width (rf/sub [:dimensions/window-width]))
 
 (def container
-  {:flex-direction     :row
+  {:flex               1
+   :flex-direction     :row
    :align-items        :center
    :justify-content    :space-between
-;;   :background-color   :red
    :padding-horizontal 10
    :margin-bottom      20})
 
@@ -16,23 +15,24 @@
   {:flex        1
    :align-items :flex-start})
 
-(def item-container
-  {:width              300
-   :height             44
-   :flex               1
-   :flex-direction     :column
-   :align-items        :flex-start
-   :border-radius      12
-   :border-width       1
-;;   :border-color       :green
-   :padding-horizontal 8
-   :padding-vertical   5
-;;  :background-color   :grey
-  })
+
+(defn item-container
+  [border-color window-width]
+  {:width           (/ window-width 3)
+   :height          44
+   :flex            1
+   :flex-direction  :column
+   :align-items     :flex-start
+   :justify-content :center
+   :margin-bottom   12
+   :border-color    border-color
+   :border-width    1
+   :border-radius   12})
 
 (def item-row
-  {:flex-direction :row
-   :align-items    :center})
+  {:flex             1
+   :flex-direction  :row
+   :align-items      :center})
 
 (def amount
   {:margin-right 4})
@@ -41,12 +41,11 @@
   {:margin-right 4})
 
 (def icon-container
-  {:width           20
-   :height          20
-   :margin-right    4
+  {:width            20
+   :height           20
+   :margin-right     4
    :align-items     :flex-start
-   :justify-content :center
-
+   :justify-content  :center
   })
 
 (def icon
--- src/quo2/components/network_connections/view.cljs
+++ src/quo2/components/network_connections/view.cljs
@@ -4,33 +4,35 @@
     [quo2.components.network-connections.style :as style]
     [status-im.ui.components.fast-image :as fast-image]
     [taoensso.timbre :as log]
-  ))
+    [status-im.ui.components.react :as react]
+    [utils.re-frame :as rf]
+    [status-im2.contexts.onboarding.common.background.view :as background]))
 
-(defn network-connections-item-render
+(defn- network-connections-item-render
   "This component based on the following properties:
   - :item - item which got color, amount, currency, icon, type properties 
   "
-  [{:keys [amount type currency icon connectedTo? connectedFrom? id color]}]
+  [{:keys [amount type currency icon connectedTo? connectedFrom? id color]} window-width]
   [rn/view
-   {:style               [(style/item-container {:border-color color})]
-    :accessibility-label :network-connections-item-container}
+   {:style (style/item-container color window-width)}
    [rn/view
     {:style [style/item-row]}
     [rn/text
      {:style [style/amount]} amount]
     [rn/text
-     {:style [style/currency]} currency]]]
-  [rn/view
-   {:style [style/item-row]}
+     {:style [style/currency]} currency]]
    [rn/view
-    {:style [style/icon-container]}
-    [fast-image/fast-image
-     {:style       style/icon
-      :resize-mode :contain
-      :source      icon}]
-   ]
-   [rn/text
-    {:style [style/title]} type]])
+    {:style [style/item-row]}
+    [rn/view
+     {:style [style/icon-container]}
+     [fast-image/fast-image
+      {:style       style/icon
+       :resize-mode :contain
+       :source      icon}]]
+    [rn/text
+     {:style [style/title]} type]]
+   ])
+
 
 (defn network-connections
   "This component based on the following properties:
@@ -38,29 +40,23 @@
   - :to - vector of to connections
   "
   [{:keys [from to]}]
-  [rn/view
-   {:style               style/container
-    :accessibility-label :network-connections-container}
-   [rn/view
-    {:style               style/column
-     :accessibility-label :network-connections-column-left}
-    [rn/text "From"]
-    [rn/flat-list
-     {:flex                         1
-      :keyboard-should-persist-taps :always
-      :data                         from
-      :listKey                      "From Column"
-      :render-fn                    network-connections-item-render
-      :key-fn                       :id}]]
-   [rn/view
-    {:style               style/column
-     :accessibility-label :network-connections-column-right}
-    [rn/text "To"]
 
-    [rn/flat-list
-     {:flex                         1
-      :keyboard-should-persist-taps :always
-      :data                         to
-      :listKey                      "To Column"
-      :render-fn                    network-connections-item-render
-      :key-fn                       :id}]]])
+  (let [window-width (rf/sub [:dimensions/window-width])]
+    [rn/view
+     {:style               style/container
+      :accessibility-label :network-connections-container}
+     [rn/view
+      {:style               style/column
+       :accessibility-label :network-connections-column-left}
+      [rn/text "From"]
+      [react/scroll-view
+       (for [network-connection-from-item from]
+         [network-connections-item-render network-connection-from-item window-width])]]
+     [rn/view
+      {:style               style/column
+       :accessibility-label :network-connections-column-right}
+      [rn/text "To"]
+
+      [react/scroll-view
+       (for [network-connection-to-item to]
+         [network-connections-item-render network-connection-to-item window-width])]]]))
